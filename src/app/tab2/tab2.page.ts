2ca50cf26142fd6ddeeedf5a741663200c4fbf0a
Matheus Peixoto <mateusnascimentopxt@gmail.com>
2023-03-15 11:35:09 -0300

    animacoes e pagina de doação

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,4 +9,16 @@ export class Tab2Page {
 
   constructor() {}
 
+  donateBlood(){
+    window.open('https://www.google.com.br/maps/search/pontos+de+doa%C3%A7%C3%A3o+de+sangue', '_blank');
+  }
+
+  donateClothes(){
+    window.open('https://www.google.com.br/maps/search/pontos+de+doa%C3%A7%C3%A3o+de+roupas', '_blank');
+  }
+
+  donateFood(){
+    window.open('https://www.google.com.br/maps/search/pontos+de+doa%C3%A7%C3%A3o+de+alimentos/maps/search/pontos+de+doa%C3%A7%C3%A3o+de+roupas', '_blank');
+  }
+
 }
