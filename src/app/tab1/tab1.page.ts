e02748f5879e290f06a611bde1d33fc182620da7
Matheus Peixoto <mateusnascimentopxt@gmail.com>
2023-03-15 11:05:34 -0300

    criação do apk e inicio da pagina home

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private route: Router) {}
+
+  DonationsPage(){
+    this.route.navigate(['/tabs/tab2']);
+  }
+
+  ReceivePage(){
+    this.route.navigate(['/tabs/tab3']);
+  }
+
 
 }
